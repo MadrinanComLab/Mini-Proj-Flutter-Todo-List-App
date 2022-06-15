e64dcc5173d1b0ebd2165ed7a5512bac05611e34
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-15 22:01:36 +0700

    Initial code needed in notes_database.dart was created

---
+++ lib/db/notes_database.dart
@@ -0,0 +1,34 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:exp_flutter_sqlite_crud/model/note.dart';
+
+class NotesDatabase {
+  static final NotesDatabase instance = NotesDatabase._init(); // GLOBAL FIELD INSTANCE
+  static Database? _database;
+  NotesDatabase._init(); // PRIVATE CONSTRUCTOR
+
+  Future<Database> get database async {
+    // IF YOU ARE CONFUSE ABOUT THE "!" AT THE END OF THE VARIABLE NAME GO TO THIS LINK, IT WAS CALLED BANG OPERATOR:
+    // https://stackoverflow.com/questions/67667071/understanding-bang-operator-in-dart
+    if (_database != null) return _database!; // IF DATABASE EXIST THEN RETURN IT
+
+    // IF NOT, THEN INITIALIZED IT
+    _database = await _initDB("notes.db");
+    return _database!;
+  }
+
+  Future<Database> _initDB (String filePath) async {
+    final dbPath = await getDatabasesPath(); // THIS IS AN EXAMPLE OF HOW TO DECLARE CONSTANT IN DART.
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB (Database db, int version) async {
+
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
